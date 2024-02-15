f5ce62fd768c4f0a1c5a2c3be49f82f88851ad6b
rupertgehrlein <rupertadrian92@gmail.com>
2024-02-15 11:59:46 +0100

    Changes with spacing below the cards of "lektionen", "übungen" and "downloads" for smaller screens

--- src/app/lektionen/lektionen-home/lektionen-home.component.ts
+++ src/app/lektionen/lektionen-home/lektionen-home.component.ts
@@ -8,27 +8,4 @@ import { CommonModule } from '@angular/common';
 })
 export class LektionenHomeComponent {
 
-  lections = [
-    {
-      title: "Kryptographie",
-      subsections: ["Lektion 1", "Lektion 2", "Lektion 3"],
-      link: "/kryptographie"
-    },
-    {
-      title: "Blockchain",
-      subsections: ["Lektion 1", "Lektion 2", "Lektion 3"],
-      link: "/blockchain"
-    },
-    {
-      title: "Kryptowährungen",
-      subsections: ["Lektion 1", "Lektion 2", "Lektion 3"],
-      link: "/kryptowährungen"
-    },
-    {
-      title: "Non-Fungible Tokens",
-      subsections: ["Lektion 1", "Lektion 2", "Lektion 3"],
-      link: "/nfts"
-    },
-  ]
-
 }
