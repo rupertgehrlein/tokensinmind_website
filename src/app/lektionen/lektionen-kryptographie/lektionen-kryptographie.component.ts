591ef43097e623e49a2348a05587884be695ac5a
rupertgehrlein <rupertadrian92@gmail.com>
2024-05-15 15:29:08 +0200

    adds functioning system for tracking time but not able to see if tab is in front or browser is visible

--- src/app/lektionen/lektionen-kryptographie/lektionen-kryptographie.component.ts
+++ src/app/lektionen/lektionen-kryptographie/lektionen-kryptographie.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SupabaseService } from 'src/app/services/supabase.service';
 
 @Component({
   selector: 'app-lektionen-kryptographie',
@@ -7,5 +8,39 @@ import { CommonModule } from '@angular/common';
   styleUrl: './lektionen-kryptographie.component.scss'
 })
 export class LektionenKryptographieComponent {
+  startTime: number;
+  initialTime: number;
+  elapsedTime: number;
+  userId;
+  timer: any;
 
+  constructor(private supabaseService: SupabaseService) {}
+
+  async ngOnInit() {
+    this.userId = await this.supabaseService.getUserId();
+    this.initialTime = await this.supabaseService.getTime('lektion', 'kryptographie', this.userId) || 0;
+    this.startTime = Date.now() - this.initialTime; // Startzeit des Timers inkl. bereits verstrichener Zeit
+
+    // Starte den Timer, um die Zeit zu aktualisieren und in die Datenbank zu schreiben
+    this.timer = setInterval(() => {
+      this.updateTimeAndSaveToDatabase();
+    }, 1000); // Timer wird jede Sekunde aktualisiert
+  }
+
+  ngOnDestroy() {
+    // Stoppe den Timer, wenn die Komponente zerstört wird
+    clearInterval(this.timer);
+  }
+
+  @HostListener('window:beforeunload', ['$event'])
+  onBeforeUnload(event) {
+    clearInterval(this.timer);
+  }
+
+  updateTimeAndSaveToDatabase() {
+    // Aktualisiere die verstrichene Zeit in Sekunden
+    this.elapsedTime = Math.floor((Date.now() - this.startTime) / 1000);
+    this.supabaseService.setTime('lektion', 'kryptographie', this.initialTime + this.elapsedTime, this.userId);
+  }
 }
+
--- src/app/services/supabase.service.ts
+++ src/app/services/supabase.service.ts
@@ -36,6 +36,69 @@ export class SupabaseService {
     return capitalizedUsername;
 
   }
+
+  async getUserId() {
+    // Benutzer-Session abrufen
+    const { data: user, error: sessionError } = await this.client.auth.getSession();
+
+    if (sessionError) {
+      console.error(sessionError);
+      return null;
+    }
+
+    if (!user) {
+      console.error('Benutzer ist nicht eingeloggt');
+      return null;
+    }
+
+    const userid = user.session.user.id;
+
+    return userid;
+
+  }
+
+
+  async getTime(type, subject, userid) {
+    const column = 'time_' + type + '_' + subject;
+
+    try {
+      const { data, error } = await this.client
+        .from('usernames')
+        .select(column)
+        .eq('userid', userid);
+
+      if (error) {
+        console.error("Fehler beim Abrufen der Zeitdaten:", error);
+        return null;
+      }
+      const time = data[0][column];
+      return time;
+    } catch (error) {
+      console.error("Unbekannter Fehler:", error);
+      return null;
+    }
+  }
+
+  async setTime(type, subject, time, userid) {
+    const column = 'time_' + type + '_' + subject;
+    const setTime = time;
+
+    try {
+      const { data: time, error } = await this.client
+        .from('usernames')
+        .update({ [column]: setTime })
+        .eq('userid', userid);
+
+      if (error) {
+        console.error("Fehler beim Schreiben der Zeitdaten:", error);
+        return null;
+      }
+    } catch (error) {
+      console.error("Unbekannter Fehler:", error);
+      return null;
+    }
+  }
+
 }
 
 /* async registerNewUser(username, password) {
